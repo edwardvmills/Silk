1bb3928dd36adbed34d57b113be386e437e98549
Syres916 <46537884+Syres916@users.noreply.github.com>
2019-02-07 14:41:18 +0000

    Py3 Fixes to ControlGridNStar66_NSub_test.py

--- ControlGridNStar66_NSub_test.py
+++ ControlGridNStar66_NSub_test.py
@@ -22,9 +22,9 @@ class ControlGridNStar66_NSub:
 			fp.addProperty('App::PropertyVectorList', "Poles_%d" % i, "ControlGridNStar66_NSub", "Poles_%d" % i)
 			fp.addProperty('App::PropertyFloatList', "Weights_%d" % i, "ControlGridNStar66_NSub", "Weights_%d" % i)
 
-		print 'pull data from each linked sub grid:'
+		print ('pull data from each linked sub grid:')
 		for i, sub in enumerate(fp.SubList):
-			print "Sub Grid %d pole[0] = " % i, fp.SubList[i].Poles[0]
+			print ("Sub Grid %d pole[0] = " % i, fp.SubList[i].Poles[0])
 		# build poles and weights nested lists
 		Poles = [0]*len(fp.SubList)
 		Weights = [0]*len(fp.SubList)
@@ -38,9 +38,9 @@ class ControlGridNStar66_NSub:
 		# set Poles[i] to to Poles_i and Weights[i] to Weights_i
 		for i, sub in enumerate(fp.SubList):
 			setattr(fp, "Poles_%d" % i, Poles[i])
-			print getattr(fp, "Poles_%d" % i)
+			print (getattr(fp, "Poles_%d" % i))
 			setattr(fp, "Weights_%d" % i, Weights[i])
-			print getattr(fp, "Weights_%d" % i)
+			print (getattr(fp, "Weights_%d" % i))
 
 
 sel=Gui.Selection.getSelection()
