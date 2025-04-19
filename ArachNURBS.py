50163261fd21ce07124af30f4769634bc8a9e667
edwardvmills <edwardvmills@users.noreply.github.com>
2025-04-19 14:40:37 -0700

    test github issue
    test github issue

--- ArachNURBS.py
+++ ArachNURBS.py
@@ -19,6 +19,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+#
 
 from __future__ import division # allows floating point division from integers
 import Part
