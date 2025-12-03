52e3b1b1dfc801ba23532dea9eaa3cc5d55bfce3
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2025-12-02 22:18:47 -0500

    Update Copyright Headers

--- SubGrid33_2Grid64.py
+++ SubGrid33_2Grid64.py
@@ -1,24 +1,25 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileNotice: Part of the Silk addon.
 
-
-#    (c) Edward Mills 2016-2017
-#    edwardvmills@gmail.com
-#	
-#    NURBS Surface modeling tools focused on low degree and seam continuity (FreeCAD Workbench) 
-#
-#    Silk is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+#                                                                              #
+#   (c) 2016 Edward Mills <edwardvmills@gmail.com>                             #
+#                                                                              #
+#   Silk is free software: you can redistribute it and/or modify it            #
+#   under the terms of the GNU General Public License as published by          #
+#   the Free Software Foundation, either version 3 of the License, or          #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   This program is distributed in the hope that it will be useful,            #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                       #
+#                                                                              #
+#   See the GNU General Public License for more details.                       #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with this program. If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                              #
+################################################################################
 
 
 from __future__ import division # allows floating point division from integers
