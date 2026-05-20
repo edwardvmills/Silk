39255bdc4fef28dfe27c619d6955ffb65f4a96cf
edwardvmills <edwardvmills@users.noreply.github.com>
2026-05-20 03:40:49 -0700

    Update ControlGrid44_2EdgeSegments.py
    batch operation:
    outputs 5 segment grids
    inputs: surface, edge1_segmenta, edge1_segmentb, edge1_segmentc, edge2_segmenta, edge2_segmentb, edge2_segmentc,

--- ControlGrid44_2EdgeSegments.py
+++ ControlGrid44_2EdgeSegments.py
@@ -36,16 +36,60 @@ path_Silk = os.path.dirname(Silk_dummy.__file__)
 path_Silk_icons =  os.path.join( path_Silk, 'Resources', 'Icons')
 iconPath = path_Silk_icons + '/ControlGrid44_2EdgeSegments.svg'
 
+def makeSingle(surface,curve_a,curve_b):
+	a=FreeCAD.ActiveDocument.addObject("Part::FeaturePython","ControlGrid44_2EdgeSegments_000")
+	AN.ControlGrid44_2EdgeSegments(a,surface,curve_a,curve_b)
+	a.ViewObject.Proxy=0 # just set it to something different from None (this assignment is needed to run an internal notification)
+	a.ViewObject.LineWidth = 1.00
+	a.ViewObject.LineColor = (0.0,170/255,255/255)
+	a.ViewObject.PointSize = 2.00
+	a.ViewObject.PointColor = (0.0,85/255,255/255)
+	return
+
 class ControlGrid44_2EdgeSegments():
 	def Activated(self):
 		sel=Gui.Selection.getSelection()
 		if len(sel)==0:
 			tipsDialog("Silk: ControlGrid44_2EdgeSegments", moreInfo)
 			return
+		if len(sel)!=3 and len(sel)!=7:
+			tipsDialog("Silk: ControlGrid44_2EdgeSegments", 
+			  "invalid input, click the icon with nothing selected for a tooltip. \n\n"
+			  "select: one surface and two curve segments on adjacent sides, \n"
+			  "or: one surface and 3 curve segments on two adjacent sides \n"
+			  "(3 consecutive segments for each side)")
+			return
 		
-		surface=Gui.Selection.getSelection()[0]
-		curve_a=Gui.Selection.getSelection()[1]
-		curve_b=Gui.Selection.getSelection()[2]
+		if len(sel)==3:
+			print("in single")
+			surface=Gui.Selection.getSelection()[0]
+			curve_a=Gui.Selection.getSelection()[1]
+			curve_b=Gui.Selection.getSelection()[2]
+			makeSingle(surface,curve_a,curve_b)
+
+		if len(sel)==7:
+			# check types later?
+			surface=Gui.Selection.getSelection()[0]
+			# side 1
+			curve_a0=Gui.Selection.getSelection()[1]
+			curve_a1=Gui.Selection.getSelection()[2]
+			curve_a2=Gui.Selection.getSelection()[3]
+			# side 2
+			curve_b0=Gui.Selection.getSelection()[4]
+			curve_b1=Gui.Selection.getSelection()[5]
+			curve_b2=Gui.Selection.getSelection()[6]
+			# middle piece
+			makeSingle(surface,curve_a1,curve_b1)
+			# bottom
+			makeSingle(surface,curve_a1,curve_b0)
+			# right
+			makeSingle(surface,curve_a2,curve_b1)
+			# top
+			makeSingle(surface,curve_a1,curve_b2)
+			# left
+			makeSingle(surface,curve_a0,curve_b1)
+
+		'''
 		a=FreeCAD.ActiveDocument.addObject("Part::FeaturePython","ControlGrid44_2EdgeSegments_000")
 		AN.ControlGrid44_2EdgeSegments(a,surface,curve_a,curve_b)
 		a.ViewObject.Proxy=0 # just set it to something different from None (this assignment is needed to run an internal notification)
@@ -53,6 +97,8 @@ class ControlGrid44_2EdgeSegments():
 		a.ViewObject.LineColor = (0.0,170/255,255/255)
 		a.ViewObject.PointSize = 2.00
 		a.ViewObject.PointColor = (0.0,85/255,255/255)
+		'''
+
 		FreeCAD.ActiveDocument.recompute()
 	
 	def GetResources(self):
